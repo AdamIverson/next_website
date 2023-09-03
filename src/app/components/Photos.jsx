6c665f653adb8c8e52ebf40ca5634b5bf1d3e0bb
Adam Iverson <aciverso@gmail.com>
2023-09-03 09:45:05 -0500

    mockdata into photos component

--- src/app/components/Photos.jsx
+++ src/app/components/Photos.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Box } from '@mui/system'
+import './photos.css'
+import jsonData from '../public/jsonData'
 
 function Photos() {
+  console.log("photos json", jsonData )
   return (
-    <Box>Photos</Box>
+    <Box id="photoViewer">Photos</Box>
   )
 }
 
