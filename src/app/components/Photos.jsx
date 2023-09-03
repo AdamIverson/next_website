3e7414fc5edde1c27296bf79302c05232752b1ba
Adam Iverson <aciverso@gmail.com>
2023-09-03 14:03:25 -0500

    mui-image installed, photo grid displaying

--- src/app/components/Photos.jsx
+++ src/app/components/Photos.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import { Box } from '@mui/system'
+import { Image } from 'mui-image'
 import './photos.css'
 import jsonData from '../public/jsonData'
 
 function Photos() {
-  console.log("photos json", jsonData )
   return (
-    <Box id="photoViewer">Photos</Box>
+    <Box>
+      <h1>Photos</h1>
+      <Image></Image>
+      <Box id="photoViewer">
+        {jsonData.map((item) => {
+          return (
+            <Image 
+              key={item.id} 
+              className="photo"
+              src="http://www.adamiversonphotography.com/uploads/1/0/7/0/107020049/prime-hatclub-2022-1_orig.jpg"
+              >
+              {/* <Box>
+                {item.id}
+              </Box>
+              <Box>
+                {item.name}
+              </Box> */}
+            </Image>
+          )
+        })}
+      </Box>
+    </Box>
   )
 }
 
